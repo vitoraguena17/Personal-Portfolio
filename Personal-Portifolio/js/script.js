93681238a356fc0631f80206983826186fe49338
Vitor Aguena <144974439+vitoraguena17@users.noreply.github.com>
2024-04-08 22:13:15 -0300

    Add files via upload

---
+++ Personal-Portifolio/js/script.js
@@ -0,0 +1,60 @@
+//*******************************************MOBILE_MENU************************************************\\
+//declarando as variáveis e puxando elas do html por id
+//declarei como const pq nao pretendo mudar o nome dela
+const btnMobile = document.getElementById("menu-mobile-btn");
+const menuListBtns = document.querySelectorAll(".menu-list-btn");
+
+function toggleMenu(event) {
+    if (event.type == 'touchstart') event.preventDefault();
+    const nav = document.getElementById("menu");
+    nav.classList.toggle('active');
+}
+//function sao codigos que podemos usar mais de uma vez
+
+btnMobile.addEventListener('click', toggleMenu);
+btnMobile.addEventListener('touchstart', toggleMenu);
+
+menuListBtns.forEach(btn => {
+    btn.addEventListener('click', toggleMenu);
+});
+
+//*******************************************DARK_MODE_BTN************************************************\\
+
+const $html = document.querySelector('html')
+const $checkbox = document.querySelector('#chk')
+
+$checkbox.addEventListener('change', function () {
+  $html.classList.toggle('dark-mode')
+})
+
+//*******************************************SCROLL_EFFECT************************************************\\
+
+const myObserver = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show')
+    } else {
+      entry.target.classList.remove('show')
+    }
+  })
+})
+
+const elements = document.querySelectorAll('.hidden')
+
+elements.forEach((element) => myObserver.observe(element))
+
+//*******************************************SLIDER_PROJECT************************************************\\
+
+var swiper = new Swiper(".portifolio-content", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
